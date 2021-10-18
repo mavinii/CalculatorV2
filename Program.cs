63f712b59654f5b7a099307b99f20bc0ecf5da77
mavinii <pg.marcosoliveira@icloud.com>
2021-10-18 16:15:04 +0100

    if and else statments added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace CalculatorV2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("**** Calculator V2 ****");
+
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Enter Operator +, -, /, *: ");
+            string op = Console.ReadLine();
+            
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            } else if (op == "-"){
+                Console.WriteLine(num1 - num2);
+            } else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            } else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
         }
     }
 }
